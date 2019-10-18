70c59b06bc1f813b712c5d203f68d1bc61034803
Pavel Batov <pba.softevol@gmail.com>
2019-10-18 16:46:28 +0300

    Add function for establishing browser connection via WebSocket protocol

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 'use strict';
 
+// Bootstrap app configs
+Object.defineProperties(global, {
+  browser: {
+    value: require('./configs/browser'),
+  },
+});
+
+const createBrowserConnection = require('./lib/browser');
+
+(async () => {
+  const browser = await createBrowserConnection();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.linkedin.com/in/pavlo-batov-7b8b0b192/');
+})();
