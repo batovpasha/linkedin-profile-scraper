c685b5dbeb957a2a9665f83fe0360e3f4b214e94
Pavel Batov <pba.softevol@gmail.com>
2019-10-21 11:43:14 +0300

    Format index.js file

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
 
 // Bootstrap app configs
-global.env = Object.defineProperties({}, {
-  browser: {
-    value: require('./configs/browser'),
-  },
-});
+global.env = Object.defineProperties(
+  {},
+  {
+    browser: {
+      value: require('./configs/browser'),
+    },
+  }
+);
 
 const createBrowserConnection = require('./lib/browser');
 
-createBrowserConnection()
-  .then(browser => browser.newPage())
-  .then(page =>
-    page.goto('https://www.linkedin.com/in/pavlo-batov-7b8b0b192/'),
-  );
+(function main() {
+  createBrowserConnection()
+    .then(browser => browser.newPage())
+    .then(page =>
+      page.goto('https://www.linkedin.com/in/pavlo-batov-7b8b0b192/')
+    );
+})();
